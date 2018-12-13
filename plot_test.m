28c0d2d080d5d1d1113d73691acdb775ace7914a
Patrick Musau <patrick.musau17@gmail.com>
2018-12-13 11:11:27 -0600

    Added Picture and Video

--- plot_test.m
+++
@@ -1,9 +0,0 @@
-clc; clear all;
-load('input_signal.mat')
-load('encoder_reading.mat')
-
-t = encoder_readings(1,:);
-enc = encoder_readings(2,:);
-inp = input_voltage_readings(2,:);
-
-plot(t,enc,t,inp)
